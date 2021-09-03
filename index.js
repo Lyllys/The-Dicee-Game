b344c4a613c31c82799c9f87e2a12346e280aed1
Lyllys Galhardo <lyllysgalhardo@gmail.com>
2021-09-02 21:52:48 -0300

    Subindo o projeto The Dicee Game

---
+++ index.js
@@ -0,0 +1,25 @@
+var randomPlayer1 = Math.floor((Math.random() * 6) +1); 
+
+for(let i = 0; i < randomPlayer1 ; i++) {
+let div = document.createElement('div');
+div.classList.add("balls");
+let diceePlayer1 =document.querySelector("#player1");
+diceePlayer1.appendChild(div);
+}
+
+var randomPlayer2 = Math.floor((Math.random() * 6) +1); 
+
+for(let i = 0; i < randomPlayer2 ; i++) {
+    let div = document.createElement('div');
+    div.classList.add("balls");
+    let diceePlayer2 =document.querySelector("#player2");
+    diceePlayer2.appendChild(div);
+}
+
+if(randomPlayer1 > randomPlayer2) {
+    document.querySelector("h1").innerHTML = "🚩 JOGADOR 1 GANHOU";
+} else if(randomPlayer2 > randomPlayer1) {
+    document.querySelector("h1").innerHTML = "JOGADOR 2 GANHOU 🚩";
+} else{
+    document.querySelector("h1").innerHTML = "EMPATE";
+}
